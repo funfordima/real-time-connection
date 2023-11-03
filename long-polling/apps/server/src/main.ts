178a8152885e450af8395252ddc484bf5091bad7
DmytroLitvinov <d.g.litvinov@gmail.com>
2023-11-03 11:33:17 +0200

    feature: create long-polling express server

---
+++ long-polling/apps/server/src/main.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import * as path from 'path';
+import events from 'events';
+
+const app = express();
+const emitter = new events.EventEmitter();
+const PORT = process.env.PORT || 3333;
+
+app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api/get-messages', (req, res) => {
+  emitter.once('newMessage', (message) => {
+    res.json(message);
+  });
+});
+
+app.post('/api/new-messages', (req, res) => {
+  const message = req.body;
+
+  emitter.emit('newMessage', message);
+
+  res.status(200);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}/api`);
+});
+
+server.on('error', console.error);
